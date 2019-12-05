2fd400480b971c55ccccd9235ed50a774171598d
Lucas Ciruzzi <hello@lukeshiru.me>
2019-12-04 22:51:37 -0300

    :ambulance: update peerDependencies (they should update automatically). :sparkles: add peer script to update peerDependencies.

---
+++ src/updatePeerDependencies.ts
@@ -0,0 +1,24 @@
+import { readFileSync, writeFileSync } from "fs";
+import { resolve } from "path";
+
+const encoding = "utf8";
+const path = resolve(__dirname, "../package.json");
+const file = readFileSync(path, encoding);
+const data = JSON.parse(file);
+const update = JSON.stringify(
+	{
+		...data,
+		peerDependencies: Object.keys(data.peerDependencies).reduce(
+			(peerDependencies, peerDependency) => ({
+				...peerDependencies,
+				[peerDependency]: data.devDependencies[peerDependency]
+			}),
+			{}
+		)
+	},
+	// eslint-disable-next-line no-null/no-null
+	null,
+	2
+);
+
+export default writeFileSync(path, update);
